5ee5129e8717f5bd45181f9a23ac315ec86b9056
andrejborstnik <andrej-borstnik@hotmail.com>
2020-07-15 15:25:57 +0200

    chore: return an empty object for Android

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * @providesModule react-native-icloudstore
  */
 
-import { AsyncStorage, NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from 'react-native'
 
-const iCloudStorage = Platform.OS === 'ios' ? NativeModules.RNICloudStorage : AsyncStorage;
+const iCloudStorage = Platform.OS === 'ios' ? NativeModules.RNICloudStorage : {}
 
-export default iCloudStorage;
-
+export default iCloudStorage
