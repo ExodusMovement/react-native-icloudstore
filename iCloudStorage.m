fe039e95ebacd9cb3fc8b3b894478563c6753764
Guten Ye <ywzhaifei@gmail.com>
2021-04-23 16:45:00 +0800

    fix: shorten ICLOUDSTORAGE_PREFIX

--- iCloudStorage.m
+++ iCloudStorage.m
@@ -10,7 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTEventDispatcher.h>
 
-static NSString* const ICLOUDSTORAGE_PREFIX = @"@com.exodusmovement.exodus.iCloudStorage/";
+static NSString* const ICLOUDSTORAGE_PREFIX = @"@com.exodus/";
 static NSString* const ICLOUD_STORE_CHANGED = @"ICLOUD_STORE_CHANGED";
 static NSString* const kStoreChangedEvent = @"iCloudStoreDidChangeRemotely";
 static NSString* const kChangedKeys = @"changedKeys";
